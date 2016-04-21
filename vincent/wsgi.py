de20b1e6b065cf220868e91ef32455542581518c
bahoo <jon.c.culver@gmail.com>
2016-04-21 10:00:10 -0700

    WhiteNoise.

--- vincent/wsgi.py
+++ vincent/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vincent.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
