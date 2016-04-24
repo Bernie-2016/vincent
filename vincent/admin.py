a585c12fead0bb2d34e7f2b6f9d380b7e36fc747
bahoo <jon.c.culver@gmail.com>
2016-04-23 18:18:23 -0700

    Attach IRs to a location + make the admin list more helpful

--- vincent/admin.py
+++ vincent/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
+from django.template.defaultfilters import truncatewords
+from django.utils.html import format_html
 from localflavor.us.us_states import US_STATES
 from .models import *
 
@@ -45,9 +47,14 @@ class CountyAdmin(OSMGeoAdmin):
     search_fields = ['county']
 
 
+class IncidentReportInline(admin.StackedInline):
+    model = IncidentReport
+
+
 @admin.register(GeocodedPollingLocation)
 class GeocodedPollingLocationAdmin(OSMGeoAdmin):
     actions = None
+    inlines = [IncidentReportInline]
     list_display = ['pollinglocation', 'addr', 'city', 'state', 'zip']
     list_filter = ['state', CountyFilter]
     search_fields = ['pollinglocation', 'precinctcode', 'addr', 'city', 'state', 'zip']
@@ -62,10 +69,19 @@ class IncidentReportAdmin(admin.ModelAdmin):
     actions = None  # this will need changing.
     fields = ['nature', 'long_line', 'scope', 'polling_location', 'reporter_name', 'reporter_phone', 'assignee', 'status', 'creator_name', 'creator_email', 'creator_phone', 'description']
     inlines = [CommentInline]
-    list_display = ['__unicode__', 'scope', 'polling_location', 'assignee', 'status']
+    list_display = ['summary', 'assignee', 'status']
+    list_display_links = ['summary']
     list_filter = ['polling_location__state', IncidentReportCountyFilter, 'long_line', 'assignee', 'status']
     raw_id_fields = ['polling_location']
     search_fields = ['nature', 'description', 'polling_location__precinctcode', 'polling_location__addr', 'polling_location__city', 'polling_location__state', 'polling_location__zip']
+
+    def summary(self, obj):
+        return format_html('<h3><a href="{}">#{} &mdash; {}</a></h3><p><strong>{}</strong></p><p style="font-weight: normal;">{}</p>',
+                        obj.get_absolute_url(),
+                        obj.pk,
+                        obj.get_nature_display(),
+                        obj.polling_location,
+                        truncatewords(obj.description, 60))
 
 
 @admin.register(PhoneNumber)
